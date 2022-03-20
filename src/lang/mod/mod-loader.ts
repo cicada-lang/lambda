6decf9f8f0e4418e363fd88048ec1f278dd5ce2a
Xie Yuheng <xyheme@gmail.com>
2022-03-20 15:23:36 +0800

    up

--- src/lang/mod/mod-loader.ts
+++ src/lang/mod/mod-loader.ts
@@ -1,26 +1,13 @@
 import { ParsingError } from "@cicada-lang/sexp/lib/errors"
-import axios from "axios"
+import { builtinUrlLoaders, UrlLoader } from "../../ut/url-loader"
 import { Mod } from "../mod"
 import { Parser } from "../parser"
-
-export interface UrlLoader {
-  (url: URL): Promise<string>
-}
-
-async function loadHttp(url: URL): Promise<string> {
-  const { data } = await axios.get(url.href)
-  return data
-}
 
 export class ModLoader {
   urlLoaders: Record<string, UrlLoader>
 
   constructor(options?: { urlLoaders?: Record<string, UrlLoader> }) {
-    this.urlLoaders = {
-      "http:": loadHttp,
-      "https:": loadHttp,
-      ...(options?.urlLoaders || {}),
-    }
+    this.urlLoaders = { ...builtinUrlLoaders, ...(options?.urlLoaders || {}) }
   }
 
   async loadText(url: URL): Promise<string> {
