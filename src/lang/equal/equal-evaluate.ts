e3d6b9c1f630ec9e84684441f3218d93bf72fb88
Xie Yuheng <xyheme@gmail.com>
2022-03-24 12:58:15 +0800

    up

--- src/lang/equal/equal-evaluate.ts
+++ src/lang/equal/equal-evaluate.ts
@@ -10,19 +10,19 @@ export function equalEvaluate(
   left: { mod: Mod; env: Env; exp: Exp },
   right: { mod: Mod; env: Env; exp: Exp }
 ): boolean {
-  // if (left.exp instanceof Exps.Ap && right.exp instanceof Exps.Ap) {
-  //   return equalApply(
-  //     ctx,
-  //     {
-  //       target: left.exp.target.evaluate(left.mod, left.env),
-  //       arg: new Exps.LazyValue(left.mod, left.env, left.exp.arg),
-  //     },
-  //     {
-  //       target: right.exp.target.evaluate(right.mod, right.env),
-  //       arg: new Exps.LazyValue(right.mod, right.env, right.exp.arg),
-  //     }
-  //   )
-  // }
+  if (left.exp instanceof Exps.Ap && right.exp instanceof Exps.Ap) {
+    return equalApply(
+      ctx,
+      {
+        target: left.exp.target.evaluate(left.mod, left.env),
+        arg: new Exps.LazyValue(left.mod, left.env, left.exp.arg),
+      },
+      {
+        target: right.exp.target.evaluate(right.mod, right.env),
+        arg: new Exps.LazyValue(right.mod, right.env, right.exp.arg),
+      }
+    )
+  }
 
   const leftValue = left.exp.evaluate(left.mod, left.env)
   const rightValue = right.exp.evaluate(right.mod, right.env)
