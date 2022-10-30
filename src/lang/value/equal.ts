d8290cb43f729634fcc4e6c4776b72eca32218bf
Xie Yuheng <xyheme@gmail.com>
2022-10-30 12:45:40 +0800

    up

--- src/lang/value/equal.ts
+++ src/lang/value/equal.ts
@@ -2,12 +2,16 @@ import { EqualCtx, Value } from "../value"
 
 export function equal(ctx: EqualCtx, left: Value, right: Value): boolean {
   if (left.preEqual !== undefined) {
-    return equal(ctx, left.preEqual(), right)
+    return equal(ctx, prepare(left), right)
   }
 
   if (right.preEqual !== undefined) {
-    return equal(ctx, left, right.preEqual())
+    return equal(ctx, left, prepare(right))
   }
 
   return left.equal(ctx, right)
+}
+
+function prepare(value: Value): Value {
+  return value.preEqual === undefined ? value : value.preEqual()
 }
