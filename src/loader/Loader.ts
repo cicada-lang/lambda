d8df77f73b8a3f42690d602f1b4b41644c6b1eca
Xie Yuheng <xyheme@gmail.com>
2024-02-27 13:36:24 +0800

    [lang0] inline `Script` to `Loader`

--- src/loader/Loader.ts
+++ src/loader/Loader.ts
@@ -1,13 +1,14 @@
 import { Fetcher } from "@cicada-lang/framework/lib/fetcher/index.js"
-import { createMod, type Mod } from "../lang0/mod/index.js"
-import { Script } from "../script/index.js"
+import * as Errors from "../lang0/errors/index.js"
+import { createMod, modExecuteStmts, type Mod } from "../lang0/mod/index.js"
+import { Parser } from "../lang0/syntax/index.js"
 
 export interface LoaderOptions {
   onOutput?: (output: string) => void
 }
 
 export class Loader {
-  cache: Map<string, Script> = new Map()
+  cache: Map<string, { mod: Mod; text: string }> = new Map()
   fetcher = new Fetcher()
 
   constructor(public options: LoaderOptions) {}
@@ -18,9 +19,23 @@ export class Loader {
 
     const text = options?.text || (await this.fetcher.fetch(url))
     const mod = createMod({ url, loader: this })
-    const script = new Script(mod, text)
-    await script.run()
-    this.cache.set(url.href, script)
-    return script.mod
+    await run(mod, text)
+    this.cache.set(url.href, { mod, text })
+    return mod
+  }
+}
+
+async function run(mod: Mod, text: string): Promise<void> {
+  const parser = new Parser()
+
+  try {
+    const stmts = parser.parseStmts(text)
+    await modExecuteStmts(mod, stmts)
+  } catch (error) {
+    if (error instanceof Errors.ParsingError) {
+      throw new Errors.ErrorReport(error.report(text))
+    }
+
+    throw error
   }
 }
