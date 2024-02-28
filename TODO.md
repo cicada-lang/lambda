dff228a962f2e937b4dead6350d9146f8f5ae2a6
Xie Yuheng <xyheme@gmail.com>
2024-02-29 01:10:14 +0800

    [lang1] improve `formatExp` on `Ap` and `Fn`

--- TODO.md
+++ TODO.md
@@ -1,4 +1,3 @@
-[lang1] improve `formatExp` on `Ap`
 [lang1] 如果能成功，为什么 closure 可以被从 `Value` 转化为 `Exp`？
 
 - 从范畴论的角度讲？
--- src/lang1/format/formatExp.ts
+++ src/lang1/format/formatExp.ts
@@ -7,17 +7,41 @@ export function formatExp(exp: Exp): string {
     }
 
     case "Fn": {
-      return `(lambda (${exp.name}) ${formatExp(exp.ret)})`
+      const { names, ret } = formatFn([exp.name], exp.ret)
+      return `(lambda (${names.join(" ")}) ${ret})`
     }
 
     case "Ap": {
-      return `(${formatExp(exp.target)} ${formatExp(exp.arg)})`
+      const { target, args } = formatAp(exp.target, [formatExp(exp.arg)])
+      return `(${target} ${args.join(" ")})`
     }
 
     case "Let": {
       const bindings = exp.bindings.map(formatBinding)
       return `(let (${bindings.join(" ")}) ${formatExp(exp.body)})`
     }
+  }
+}
+
+function formatFn(
+  names: Array<string>,
+  ret: Exp,
+): { names: Array<string>; ret: string } {
+  if (ret["@kind"] === "Fn") {
+    return formatFn([...names, ret.name], ret.ret)
+  } else {
+    return { names, ret: formatExp(ret) }
+  }
+}
+
+function formatAp(
+  target: Exp,
+  args: Array<string>,
+): { target: string; args: Array<string> } {
+  if (target["@kind"] === "Ap") {
+    return formatAp(target.target, [formatExp(target.arg), ...args])
+  } else {
+    return { target: formatExp(target), args }
   }
 }
 
