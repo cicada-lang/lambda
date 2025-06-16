fa4da8b3475f37b60a8d37f4ce512364d81a9477
Xie Yuheng <xyheme@gmail.com>
2025-06-16 14:34:14 +0800

    up

--- TODO.md
+++ TODO.md
@@ -1,5 +1,3 @@
-re-org our way of writing interpreter -- minimize dependencies
-
 # x-data
 
 use x-data -- to replace sexp
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-#!/usr/bin/env -S node --no-warnings
+#!/usr/bin/env -S node
 
 import { Commander } from "@xieyuheng/commander"
 import { runCommand } from "./commands/runCommand.ts"
