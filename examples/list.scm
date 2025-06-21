b03359b894ed4310aacf4d8498be0456a60b8b54
Xie Yuheng <xyheme@gmail.com>
2025-06-21 10:56:55 +0800

    up

--- examples/list.scm
+++ examples/list.scm
@@ -1,14 +1,14 @@
-(define null
-  (lambda (null-case cons-case)
-    null-case))
+(define nil
+  (lambda (nil-case li-case)
+    nil-case))
 
-(define (cons head tail)
-  (lambda (null-case cons-case)
-    (cons-case head tail
-      (tail null-case cons-case))))
+(define (li head tail)
+  (lambda (nil-case li-case)
+    (li-case head tail
+      (tail nil-case li-case))))
 
-(define (rec-List target null-case cons-case)
-  (target null-case cons-case))
+(define (rec-List target nil-case li-case)
+  (target nil-case li-case))
 
 (import zero add1 "./nat-church.scm")
 
@@ -20,20 +20,20 @@
 
 (import true "./boolean.scm")
 
-(assert-equal (length null) zero)
-(assert-equal (length (cons true null)) (add1 zero))
-(assert-equal (length (cons true (cons true null))) (add1 (add1 zero)))
+(assert-equal (length nil) zero)
+(assert-equal (length (li true nil)) (add1 zero))
+(assert-equal (length (li true (li true nil))) (add1 (add1 zero)))
 
 (define (append left right)
   (rec-List left
     right
     (lambda (head target almost)
-      (cons head almost))))
+      (li head almost))))
 
-(assert-equal (append null null) null)
-(assert-equal (append null (cons true null)) (cons true null))
-(assert-equal (append (cons true null) null) (cons true null))
-(assert-equal (append (cons true null) (cons true null)) (cons true (cons true null)))
-(assert-equal (append (cons true (cons true null))
-                      (cons true (cons true null)))
-              (cons true (cons true (cons true (cons true null)))))
+(assert-equal (append nil nil) nil)
+(assert-equal (append nil (li true nil)) (li true nil))
+(assert-equal (append (li true nil) nil) (li true nil))
+(assert-equal (append (li true nil) (li true nil)) (li true (li true nil)))
+(assert-equal (append (li true (li true nil))
+                      (li true (li true nil)))
+              (li true (li true (li true (li true nil)))))
