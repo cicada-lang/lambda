e8e8962958c974f32c79c565ebda49c2a7860f24
Xie Yuheng <xyheme@gmail.com>
2025-06-21 20:48:31 +0800

    up

--- examples/combinators.scm
+++ examples/combinators.scm
@@ -1,7 +1,12 @@
-(define (S x y z) ((x z) (y z)))
+(define (S f g x) ((f x) (g x)))
 (define (K x y) x)
 (define (I x) x)
 
 (I)
 (K I)
 (S K I)
+
+(define (C f x y) (f y x))
+(define (B f g x) (f (g x)))
+
+(assert-equal (K I) (C K))
